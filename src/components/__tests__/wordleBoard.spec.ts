b0dd80c74de7bfb3de054813360b04fbcf1cddec
svefur <124247388+svefur@users.noreply.github.com>
2026-04-16 10:32:39 +0200

    Only render feedback after the game is over

--- src/components/__tests__/wordleBoard.spec.ts
+++ src/components/__tests__/wordleBoard.spec.ts
@@ -1,19 +1,44 @@
-import { VICTORY_MESSAGE } from '@/settings'
+import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from '@/settings'
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
 
+const wordOfTheDay = 'TESTS'
 describe('Wordle board', () => {
 	it('a victory message appers when the user makes a guess that matches the word of the day', async () => {
 		const wrapper = mount(WordleBoard, {
 			props: {
-				wordOfTheDay: 'TESTS',
+				wordOfTheDay: wordOfTheDay,
 			},
 		})
 
 		const guessInput = wrapper.find('input[type=text]')
-		await guessInput.setValue('TESTS')
+		await guessInput.setValue(wordOfTheDay)
 		await guessInput.trigger('keydown.enter')
 
 		expect(wrapper.text()).toContain(VICTORY_MESSAGE)
+	})
+
+	test('a defeat message appers if the user makes a guess that is incorrect', async () => {
+		const wrapper = mount(WordleBoard, {
+			props: {
+				wordOfTheDay: wordOfTheDay,
+			},
+		})
+
+		const guessInput = wrapper.find('input[type=text]')
+		await guessInput.setValue('WRONG')
+		await guessInput.trigger('keydown.enter')
+
+		expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+	})
+	test('no end-of-game message appears if the user has not yet made a guess', async () => {
+		const wrapper = mount(WordleBoard, {
+			props: {
+				wordOfTheDay: wordOfTheDay,
+			},
+		})
+
+		expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+		expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
 	})
 })
