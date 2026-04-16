b3e7c93842e4343d5dd7575cbe0c73d0db211a88
svefur <124247388+svefur@users.noreply.github.com>
2026-04-16 09:51:20 +0200

    First loop

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { VICTORY_MESSAGE } from '@/settings'
+
 defineProps<{
 	wordOfTheDay: string
 }>()
 </script>
 <template>
 	<input type="text" />
-	<p>You won!</p>
+	<p>{{ VICTORY_MESSAGE }}</p>
 </template>
--- src/components/__tests__/wordleBoard.spec.ts
+++ src/components/__tests__/wordleBoard.spec.ts
@@ -1,21 +1,19 @@
+import { VICTORY_MESSAGE } from '@/settings'
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
 
 describe('Wordle board', () => {
 	it('a victory message appers when the user makes a guess that matches the word of the day', async () => {
-		// Arrange
 		const wrapper = mount(WordleBoard, {
 			props: {
 				wordOfTheDay: 'TESTS',
 			},
 		})
 
-		// ACT
 		const guessInput = wrapper.find('input[type=text]')
 		await guessInput.setValue('TESTS')
 		await guessInput.trigger('keydown.enter')
 
-		// Assert
-		expect(wrapper.text()).toContain('You won!')
+		expect(wrapper.text()).toContain(VICTORY_MESSAGE)
 	})
 })
