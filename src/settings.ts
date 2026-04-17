014b4b3dbf22a5236f01d0a13ede553f64206374
svefur <124247388+svefur@users.noreply.github.com>
2026-04-17 10:30:07 +0200

    Allow repetition

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,4 @@
-export const VICTORY_MESSAGE = 'You won!'
+export const VICTORY_MESSAGE = '🎉 Congratulations! 🎉'
 export const DEFEAT_MESSAGE = 'Better luck next time!'
 export const WORD_SIZE = 5
+export const MAX_GUESSES_COUNT = 6
